92e993bbd93a2781ab8a2fb79b4f395f2a8b405a
osir <12292530+osir@users.noreply.github.com>
2020-08-01 21:59:46 +0200

    Complete JS rewrite

--- static/js/autoscroll.js
+++ static/js/autoscroll.js
@@ -1,13 +1,35 @@
-// Settings
-let interval = 0
-let distance = 0
-let waitTop = 0
-let waitBottom = 0
-
 // Globals
-let scrollOptions = { behavior: 'smooth' }
-let scrolling = false
+let timeout = null
 let lastUpdate = null
+let paused = false
+
+// Config
+let scrollDistance
+let intervalDelay
+let topDelay
+let bottomDelay
+
+
+const startScroll = (intervalDelayMs, scrollDistancePx, topDelayS, bottomDelayS) => {
+    // Scroll to top
+    window.scrollTo(0, 0)
+
+    // Setup globals
+    lastUpdate = Date()
+
+    // Setup shortcuts
+    document.addEventListener('keyup', shortcutHandler)
+
+    // Set config variables
+    intervalDelay = intervalDelayMs
+    scrollDistance = scrollDistancePx
+    topDelay = topDelayS
+    bottomDelay = bottomDelayS
+
+    // Start the main loop
+    setInterval(mainInterval, intervalDelay);
+}
+
 
 /*
  * Keyboard shortcuts:
@@ -15,9 +37,10 @@ let lastUpdate = null
  * R - Refresh Page
  * T - Go to top and stop there
  */
-document.addEventListener('keyup', (e) => {
+const shortcutHandler = (e) => {
+    console.log('Key pressed: ' + e.code)
     if (e.code === 'KeyP') {
-        toggleScroll()
+        paused = !paused
         e.preventDefault()
     }
     if (e.code === 'KeyR') {
@@ -25,80 +48,75 @@ document.addEventListener('keyup', (e) => {
         e.preventDefault()
     }
     if (e.code === 'KeyT') {
-        stopScroll()
+        // Warning: possible race condition
+        // since mainInterval is async, it might move one pixel after jumping to top
+        paused = true
         window.scrollTo(0, 0)
         e.preventDefault()
     }
     // else let the browser handle the key normally
-});
-
-// Set all the settings and start scrolling
-const initScroll = (ms, px, top, bottom) => {
-    interval = ms
-    distance = px
-    waitTop = top
-    waitBottom = bottom
-    lastUpdate = Date.now()
-    startScroll()
 }
 
-// Wait and continue scrolling from top
-const startScroll = () => {
-    scrolling = true
-    window.scrollTo(0, 0)
-    setTimeout(() => {
-        continueScroll()
-    }, waitTop);
-}
 
-let i
-// Starts a new interval and continues scrolling from current position
-const continueScroll = () => {
-    scrolling = true
-    i = setInterval(() => {
-        if (window.scrollY >= window.scrollMaxY) {
-            // Stop loop (interval)
-            clearInterval(i)
-            // Wait and start again
-            setTimeout(() => {
-                checkForUpdates()
-                startScroll()
-            }, waitBottom)
+const mainInterval = () => {
+    let pos = window.scrollY
+
+    // If at the top, wait before scrolling
+    if (pos === 0) {
+        // If it isn't set yet, set the timeout to some seconds in the future
+        if (timeout === null) {
+            console.log('Entered top')
+            timeout = new Date()
+            timeout.setSeconds(timeout.getSeconds() + topDelay)
+        }
+        // If the current time has passed the timeout, start scrolling
+        if (new Date() > timeout) {
+            timeout = null
+            window.scrollBy(0, scrollDistance)
         }
 
-        window.scrollBy(0, distance)
-    }, interval)
-}
+    // If the bottom was reached, wait before resetting and check for an update
+    } else if (pos === window.scrollMaxY) {
+        // If it isn't set ye, wait before resetting and check for an updatee
+        if (timeout === null) {
+            console.log('Entered bottom')
+            timeout = new Date()
+            timeout.setSeconds(timeout.getSeconds() + bottomDelay)
+            // Start checking for updates
+            checkForUpdates()
+        }
+        // If the current time has passed the timeout, reset to the top
+        if (timeout < new Date()) {
+            if (!paused) {
+                timeout = null
+                window.scrollTo(0, 0)
+            }
+        }
 
-// Stop scrolling at the current position and stay there
-const stopScroll = () => {
-    clearInterval(i)
-    i = null
-    scrolling = false
-}
-
-// Toggles scrolling
-const toggleScroll = () => {
-    console.log(scrolling)
-    if (scrolling) {
-        stopScroll()
+    // Otherwise scroll normally
     } else {
-        continueScroll()
+        if (!paused) {
+            window.scrollBy(0, scrollDistance)
+        }
     }
 }
 
+
 // Checks the server and reloads if there are new scores
 const checkForUpdates = () => {
+    console.log('Started checking for update')
     let xhr = new XMLHttpRequest()
     xhr.open('GET', '/api/lastupdate', true)
     xhr.onreadystatechange = () => {
         // If request is done, compare dates
         if (xhr.readyState == 4 && xhr.status == 200) {
-            if (new Date(lastUpdate) < new Date(xhr.response)) {
-                // Server is newer, refresh
+            console.log('Response: ' + xhr.response)
+            if (lastUpdate < new Date(xhr.response)) {
+                console.log('Server is newer, reloading...')
                 location = location
             }
         }
     }
+    console.log('Sending request')
     xhr.send()
 }
